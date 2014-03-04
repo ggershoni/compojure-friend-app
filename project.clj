eeb618ad2d884eae1acbb7cfce994c76e061e05a
Guy Gershoni <guy.gershoni@team.telstra.com>
2014-03-04 17:14:18 +1100

    Got interactive form working as per demo site :-). Now so set up DB access and new accounts etc.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [compojure "1.1.6"]
                  [com.cemerick/friend "0.2.0"]]
   :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler compojure-friend-app.handler/app}
+;;;  :ring {:handler compojure-friend-app.handler/app}
+  :ring {:handler compojure-friend-app.handler/page}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
